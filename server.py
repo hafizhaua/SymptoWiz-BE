eca4ebeaad8d3d438ebc96f6a2b650d0b1a167a8
Hafizha Ulinnuha Ahmad <hafizhaua@gmail.com>
2023-06-05 18:06:31 +0700

    feat: restructure files

--- server.py
+++ server.py
@@ -1,7 +1,20 @@
-from disease import get_disease_info, generate_df
 from flask import Flask, request, jsonify, make_response
 from flask_cors import CORS, cross_origin
 import pickle
+import json
+
+def get_disease_info(name):
+    disease_dict = json.load('disease.json')
+    return disease_dict[name]
+
+# def generate_df(symptoms):
+#     pred_data = {}
+#     for symptom_candidate in symptoms_list:
+#         pred_data[symptom_candidate] = 1 if symptom_candidate in symptoms else 0
+
+#     df = pd.DataFrame(pred_data, index=[""])
+#     return df
+
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
@@ -29,15 +42,15 @@ model = pickle.load(open("model.pkl", "rb"))
 #         }
 #     )
 
-# @app.route("/api/disease", methods=["POST", "GET"])
-# @cross_origin(supports_credentials=True)
-# def getData():
-#     data = request.get_json(force=True)
-#     disease = data["disease"]
-#     info = get_disease_info(disease)
-#     info["name"] = disease
-#     print(info)
-#     return jsonify({"status": "success", "data": info})
+@app.route("/api/disease", methods=["POST", "GET"])
+@cross_origin(supports_credentials=True)
+def getData():
+    data = request.get_json(force=True)
+    disease = data["disease"]
+    info = get_disease_info(disease)
+    info["name"] = disease
+    print(info)
+    return jsonify({"status": "success", "data": info})
 
 
 @app.route("/", methods=["GET"])
